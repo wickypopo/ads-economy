e3ec2da354bd65e34b5291bf3fe1bc05fecdac52
Felix <felix@Macmini.fritz.box>
2026-06-17 14:26:10 +0200

    ticker

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { premierBrands } from "../data/premierBrands";
 import { useLocale } from "../utils/useLocale";
+import { motion } from "framer-motion";
 
 export default function Slider() {
   const { locale, setLocale, t } = useLocale();
-  console.log(premierBrands[locale].hero.items);
+  const [num, setNum] = useState(0);
+
   const map = premierBrands[locale].hero.items.map((item) => {
     return (
-      <div className="h-full bg-slate-300 rounded shadow-xl shadow-slate-500/15 flex-shrink-0 flex flex-col p-4 text-2xl w-1/4">
+      <div className="h-full bg-slate-100 rounded-lg flex-shrink-0 flex flex-col p-4 text-2xl w-full md:w-1/3 lg:w-1/4">
         <img
           src={item.img}
           className="object-cover size-full max-h-[250px] rounded"
@@ -20,10 +23,13 @@ export default function Slider() {
   });
   return (
     <div className="w-full h-full relative flex gap-4 overflow-hidden">
-      <div className="bg-linear-to-l from-white/0 to-white/100 h-full w-25 absolute" />
-      {map}
-      {map}
-      <div className="bg-linear-to-r from-white/0 to-white/100 h-full w-25 absolute right-0" />
+      <div className="bg-linear-to-l from-white/0 to-white/100 h-full w-25 absolute hidden md:block" />
+      <motion.div className="w-full">
+        {map}
+        {map}
+      </motion.div>
+
+      <div className="bg-linear-to-r from-white/0 to-white/100 h-full w-25 absolute right-0 hidden md:block" />
     </div>
   );
   {
