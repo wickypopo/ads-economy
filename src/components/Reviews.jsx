e974dc074946fb98739a0d8faf4dd401c130c7c6
wickypopo <felixm@meise.at>
2026-06-17 23:16:21 +0200

    slider added

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,23 +4,23 @@ import { motion } from "framer-motion";
 import { reviews } from "../data/Ads";
 
 const colors = [
-  "bg-red-400",
-  "bg-orange-400",
-  "bg-amber-400",
-  "bg-yellow-400",
-  "bg-lime-400",
-  "bg-green-400",
-  "bg-emerald-400",
-  "bg-teal-400",
-  "bg-cyan-400",
-  "bg-sky-400",
-  "bg-blue-400",
-  "bg-indigo-400",
-  "bg-violet-400",
   "bg-purple-400",
-  "bg-fuchsia-400",
+  "bg-sky-400",
+  "bg-orange-400",
+  "bg-emerald-400",
   "bg-pink-400",
+  "bg-yellow-400",
+  "bg-indigo-400",
+  "bg-red-400",
+  "bg-teal-400",
+  "bg-fuchsia-400",
+  "bg-lime-400",
+  "bg-blue-400",
   "bg-rose-400",
+  "bg-amber-400",
+  "bg-cyan-400",
+  "bg-violet-400",
+  "bg-green-400",
 ];
 
 export default function Reviews() {
